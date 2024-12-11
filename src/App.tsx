2b3e0041edda39c69cdee86b000e3c678dc6ed26
marvin <sea-son@foxmail.com>
2024-12-11 17:28:12 +0800

    feat: edit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,140 @@
-// TypeScript users only add this code
-// Import the Slate editor factory.
-import {createEditor, Editor, Element, Transforms} from 'slate'
-// Import the Slate components and React plugin.
-import {Editable, ReactEditor, RenderElementProps, Slate, withReact} from 'slate-react'
-// Import React dependencies.
-import {useCallback, useState} from 'react'
+import React, { useMemo } from 'react'
+import { isKeyHotkey } from 'is-hotkey'
+import { css } from '@emotion/css'
+import * as SlateReact from 'slate-react'
+import { Editable, withReact } from 'slate-react'
+import { createEditor, Range, Transforms } from 'slate'
+import { withHistory } from 'slate-history'
 
 const initialValue = [
     {
         type: 'paragraph',
         children: [
-            {text: '这是一个普通的段落，可以包含文字和输入框：'},
-            {type: 'input', value: '初始值', children: [{text: ''}]},
-            {text: ' 这里是输入框后面的文字。'},
+            {
+                type: 'button',
+                children: [{ text: 'editable button' }],
+            },
         ],
     },
-    {
-        type: 'paragraph',
-        children: [{text: '这是另一个段落。'}],
-    },
-];
-
-const App = () => {
-    const [editor] = useState(() => withReact(createEditor()))
-
-    // Define a rendering function based on the element passed to `props`. We use
-    // `useCallback` here to memoize the function for subsequent renders.
-    const renderElement = useCallback(props => {
-        switch (props.element.type) {
-            case 'input':
-                return <InputElement {...props} />;
-            default:
-                return <DefaultElement {...props} />
+]
+const InlinesExample = () => {
+    const editor = useMemo(
+        () => withInlines(withHistory(withReact(createEditor()))),
+        []
+    )
+    const onKeyDown = event => {
+        const { selection } = editor
+        // Default left/right behavior is unit:'character'.
+        // This fails to distinguish between two cursor positions, such as
+        // <inline>foo<cursor/></inline> vs <inline>foo</inline><cursor/>.
+        // Here we modify the behavior to unit:'offset'.
+        // This lets the user step into and out of the inline without stepping over characters.
+        // You may wish to customize this further to only use unit:'offset' in specific cases.
+        if (selection && Range.isCollapsed(selection)) {
+            const { nativeEvent } = event
+            if (isKeyHotkey('left', nativeEvent)) {
+                event.preventDefault()
+                Transforms.move(editor, { unit: 'offset', reverse: true })
+                return
+            }
+            if (isKeyHotkey('right', nativeEvent)) {
+                event.preventDefault()
+                Transforms.move(editor, { unit: 'offset' })
+                return
+            }
         }
-    }, [])
-
+    }
     return (
-        <Slate editor={editor} initialValue={initialValue}>
+        <SlateReact.Slate editor={editor} initialValue={initialValue}>
             <Editable
-                renderElement={renderElement}
-                onKeyDown={event => {
-                    if (!event.ctrlKey) {
-                        return
-                    }
-                    switch (event.key) {
-                    }
-                }}
+                renderElement={props => <Element {...props} />}
+                renderLeaf={props => <Text {...props} />}
+                placeholder="Enter some text..."
+                onKeyDown={onKeyDown}
             />
-        </Slate>
+        </SlateReact.Slate>
     )
 }
-const InputElement = (props: RenderElementProps & { element: { type: 'input'; value: string } }) => {
-    const {attributes, children, element} = props;
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const path = ReactEditor.findPath(props.editor, element);
-        Transforms.setNodes(
-            props.editor,
-            {value: event.target.value},
-            {at: path}
-        );
-    };
-
-    return (
-        <span {...attributes} contentEditable={false} style={{display: 'inline-flex', alignItems: 'center'}}>
-          <input
-              type="text"
-              value={element.value}
-              onChange={handleInputChange}
-              style={{
-                  margin: '0 5px',
-                  padding: '2px 4px',
-                  border: '1px solid #ccc',
-                  borderRadius: '4px',
-                  fontSize: 'inherit',
-              }}
-          />
-            {children}
-    </span>
-    );
-};
-
-const DefaultElement = props => {
-    return <p {...props.attributes}>{props.children}</p>
+const withInlines = editor => {
+    const { insertData, insertText, isInline, isElementReadOnly, isSelectable } =
+        editor
+    editor.isInline = element =>
+        ['button'].includes(element.type) || isInline(element)
+    return editor
 }
 
-export default App
+// Put this at the start and end of an inline component to work around this Chromium bug:
+// https://bugs.chromium.org/p/chromium/issues/detail?id=1249405
+const InlineChromiumBugfix = () => (
+    <span
+        contentEditable={false}
+        className={css`
+      font-size: 0;
+    `}
+    >
+    {String.fromCodePoint(160) /* Non-breaking space */}
+  </span>
+)
+const EditableButtonComponent = ({ attributes, children }) => {
+    return (
+        /*
+          Note that this is not a true button, but a span with button-like CSS.
+          True buttons are display:inline-block, but Chrome and Safari
+          have a bad bug with display:inline-block inside contenteditable:
+          - https://bugs.webkit.org/show_bug.cgi?id=105898
+          - https://bugs.chromium.org/p/chromium/issues/detail?id=1088403
+          Worse, one cannot override the display property: https://github.com/w3c/csswg-drafts/issues/3226
+          The only current workaround is to emulate the appearance of a display:inline button using CSS.
+        */
+        <span
+            {...attributes}
+            onClick={ev => ev.preventDefault()}
+            // Margin is necessary to clearly show the cursor adjacent to the button
+            className={css`
+                margin: 0 0.1em;
+
+                background-color: #efefef;
+                padding: 2px 6px;
+                border: 1px solid #767676;
+                border-radius: 2px;
+                font-size: 0.9em;
+            `}
+        >
+      <InlineChromiumBugfix />
+            {children}
+            <InlineChromiumBugfix />
+    </span>
+    )
+}
+const Element = props => {
+    const { attributes, children, element } = props
+    switch (element.type) {
+        case 'button':
+            return <EditableButtonComponent {...props} />
+        default:
+            return <p {...attributes}>{children}</p>
+    }
+}
+const Text = props => {
+    const { attributes, children, leaf } = props
+    return (
+        <span
+            // The following is a workaround for a Chromium bug where,
+            // if you have an inline at the end of a block,
+            // clicking the end of a block puts the cursor inside the inline
+            // instead of inside the final {text: ''} node
+            // https://github.com/ianstormtaylor/slate/issues/4704#issuecomment-1006696364
+            className={
+                leaf.text === ''
+                    ? css`
+              padding-left: 0.1px;
+            `
+                    : null
+            }
+            {...attributes}
+        >
+      {children}
+    </span>
+    )
+}
+export default InlinesExample
