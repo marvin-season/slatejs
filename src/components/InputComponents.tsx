a802af588ba78e9740626829cb6530d21e7c57be
marvin <sea-son@foxmail.com>
2024-12-13 12:19:22 +0800

    feat: setNodes

--- src/components/InputComponents.tsx
+++ src/components/InputComponents.tsx
@@ -1,13 +1,21 @@
 import { FC, useState } from 'react';
-import { RenderElementProps } from 'slate-react';
+import { ReactEditor, RenderElementProps, useSlateStatic } from 'slate-react';
+import { Transforms } from 'slate';
 
 const InputComponents: FC<RenderElementProps> = ({ attributes, element, children }) => {
   const [value, setValue] = useState(element.value);
-
+  const editor = useSlateStatic();
   return <>
     <span {...attributes} contentEditable={false}>
       <input value={value} placeholder={element.placeholder} onChange={(e) => {
-        setValue(e.target.value);
+        const v = e.target.value;
+        setValue(v);
+        const path = ReactEditor.findPath(editor, element);
+        Transforms.setNodes(editor, {
+          value: v,
+        }, {
+          at: path,
+        });
       }} />
     </span>
   </>;
